3763d9f4737441c7c1b8c6d0926207f51e7c8f2c
OguzhanCinar-01 <118293608+OguzhanCinar-01@users.noreply.github.com>
2025-06-22 22:32:39 +0300

    Fix: Add Restart project files correctly

---
+++ Restart-App/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Oğuzhan Cnr on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
